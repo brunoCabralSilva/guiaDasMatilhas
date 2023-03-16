5c1bed82bd2ecba5d06c06b4c3ff633a038c01a4
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-03-16 13:25:29 -0300

    Conclusão da página inicial e do menu principal

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,67 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Navigation() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return(
-    <nav>
-      <Link className="item-menu" to='/'> Início </Link>
-      <Link className="item-menu" to='/trybes'> Tribos </Link>
-      <Link className="item-menu" to='/auspices'> Augúrios </Link>
-      <Link className="item-menu" to='/breeds'> Raças </Link>
-      <Link className="item-menu" to='/about'> Quem Somos </Link>
-    </nav>
+    <header>
+      <nav className="default">
+        <Link className="item-menu" to='/'> Início </Link>
+        <Link className="item-menu" to='/trybes'> Tribos </Link>
+        <Link className="item-menu" to='/auspices'> Augúrios </Link>
+        <Link className="item-menu" to='/breeds'> Raças </Link>
+        <Link className="item-menu" to='/about'> Quem Somos </Link>
+      </nav>
+      <nav className="mobile">
+        <div
+          className="button-menu"
+          onClick={() => setShowMenu(!showMenu)}
+        >
+          <div className={`item ${showMenu ? 'item1-opened' : 'item1-closed'}`} />
+          <div className={`item ${showMenu ? 'item2-opened' : 'item2-closed'}`} />
+          <div className={`item ${showMenu ? 'item3-opened' : 'item3-closed'}`} />
+        </div>
+        {
+          showMenu && <div className="mobile-links">
+            <Link
+              to="/"
+              className="item-menu"
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              Início
+            </Link>
+            <Link
+              to="/trybes"
+              className="item-menu"
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              Tribos
+            </Link>
+            <Link
+              to="/auspices"
+              className="item-menu"
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              Augúrios
+            </Link>
+            <Link
+              to="/breeds"
+              className="item-menu"
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              Raças
+            </Link>
+            <Link
+              to="/about"
+              className="item-menu"
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              Quem Somos
+            </Link>
+          </div>
+        }
+      </nav>
+    </header>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return(
-    <div>
-      Home
-    </div>
+    <section className="title-home">
+      <h1 className="title-page">
+        Guia das Matilhas
+      </h1>
+      <img
+        src={require("../images/logos/arrow-down.png")}
+        alt="seta para baixo"
+        className="arrow-down"
+      />
+    </section>
   );
 }

75545a0903a876df980f3aedcc736d07a043b296
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-03-20 18:50:03 -0300

    Correção de bugs no componente de navegação

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,55 +13,53 @@ export default function Navigation() {
         <Link className="item-menu" to='/breeds'> Raças </Link>
         <Link className="item-menu" to='/about'> Sobre </Link>
       </nav>
-      <nav className={`mobile ${showMenu? 'front' : 'back'}`}>
-        <div
-          className="button-menu"
-          onClick={() => setShowMenu(!showMenu)}
-        >
-          <div className={`item ${showMenu ? 'item1-opened' : 'item1-closed'}`} />
-          <div className={`item ${showMenu ? 'item2-opened' : 'item2-closed'}`} />
-          <div className={`item ${showMenu ? 'item3-opened' : 'item3-closed'}`} />
-        </div>
-        {
-          showMenu && <div className="mobile-links">
-            <Link
-              to="/"
-              className="item-menu"
-              onClick={() => setShowMenu(!showMenu)}
-            >
-              Início
-            </Link>
-            <Link
-              to="/trybes"
-              className="item-menu"
-              onClick={() => setShowMenu(!showMenu)}
-            >
-              Tribos
-            </Link>
-            <Link
-              to="/auspices"
-              className="item-menu"
-              onClick={() => setShowMenu(!showMenu)}
-            >
-              Augúrios
-            </Link>
-            <Link
-              to="/breeds"
-              className="item-menu"
-              onClick={() => setShowMenu(!showMenu)}
-            >
-              Raças
-            </Link>
-            <Link
-              to="/about"
-              className="item-menu"
-              onClick={() => setShowMenu(!showMenu)}
-            >
-              Sobre
-            </Link>
-          </div>
-        }
-      </nav>
+      <div
+        className="button-menu front2"
+        onClick={() => setShowMenu(!showMenu)}
+      >
+        <div className={`item ${showMenu ? 'item1-opened' : 'item1-closed'}`} />
+        <div className={`item ${showMenu ? 'item2-opened' : 'item2-closed'}`} />
+        <div className={`item ${showMenu ? 'item3-opened' : 'item3-closed'}`} />
+      </div>
+      {
+        showMenu && <nav className={`${showMenu? 'front' : 'back'} mobile mobile-links`}>
+          <Link
+            to="/"
+            className="item-menu"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            Início
+          </Link>
+          <Link
+            to="/trybes"
+            className="item-menu"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            Tribos
+          </Link>
+          <Link
+            to="/auspices"
+            className="item-menu"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            Augúrios
+          </Link>
+          <Link
+            to="/breeds"
+            className="item-menu"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            Raças
+          </Link>
+          <Link
+            to="/about"
+            className="item-menu"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            Sobre
+          </Link>
+        </nav>
+      }
     </header>
   );
 }
